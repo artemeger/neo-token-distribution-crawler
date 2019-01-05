768753c95aee415f3e44b657442c40f76c2cd47d
Artem Eger <11612812+yuomii@users.noreply.github.com>
2019-01-05 22:41:14 +0100

    Create crawler.py

---
+++ crawler.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import time
+import sys
+import csv
+
+pagesToScan = 121000
+hashToFilter = '45d493a6f73fa5f404244a5fb8472fc014ca5885'
+data = dict()
+
+for pageNumber in range (1, pagesToScan, 1):
+
+	req = requests.get('https://neoscan.io/addresses/' + str(pageNumber), headers={'User-Agent': 'Mozilla/5.0'})
+	req.encoding = 'UTF-8'
+	soup = BeautifulSoup(req.text, 'html.parser')
+	print('Page number: ' +str(pageNumber))
+
+	for address in soup.find_all('a', attrs={'class':'large-blue-link col-4-width'}):
+		addressString = address.get_text()
+		reqJson = requests.get('https://api.neoscan.io/api/main_net/v1/get_balance/' + addressString, headers={'User-Agent': 'Mozilla/5.0'})
+		reqJson.encoding = 'UTF-8'
+		jsonData = json.loads(reqJson.text)
+		
+		for balance in jsonData['balance']:
+			if(balance['asset_hash'] == hashToFilter):
+				print(addressString + ' : ' + str(balance['amount']))
+				data.update({addressString : str(balance['amount'])})
+
+with open('balances.csv', 'w') as file:
+	w = csv.writer(file)
+	w.writerows(data.items())
+	
+sys.exit()
