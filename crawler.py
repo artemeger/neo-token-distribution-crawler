2a6cf2fbcc58539af619d45a322974ca5fa2d553
Artem Eger <11612812+yuomii@users.noreply.github.com>
2019-01-05 23:00:11 +0100

    Update crawler.py
    only balances bigger 20k

--- crawler.py
+++ crawler.py
@@ -11,24 +11,27 @@ data = dict()
 
 for pageNumber in range (1, pagesToScan, 1):
 
-	req = requests.get('https://neoscan.io/addresses/' + str(pageNumber), headers={'User-Agent': 'Mozilla/5.0'})
-	req.encoding = 'UTF-8'
-	soup = BeautifulSoup(req.text, 'html.parser')
-	print('Page number: ' + str(pageNumber))
+        req = requests.get('https://neoscan.io/addresses/' + str(pageNumber), headers={'User-Agent': 'Mozilla/5.0'})
+        req.encoding = 'UTF-8'
+        soup = BeautifulSoup(req.text, 'html.parser')
 
-	for address in soup.find_all('a', attrs={'class':'large-blue-link col-4-width'}):
-		addressString = address.get_text()
-		reqJson = requests.get('https://api.neoscan.io/api/main_net/v1/get_balance/' + addressString, headers={'User-Agent': 'Mozilla/5.0'})
-		reqJson.encoding = 'UTF-8'
-		jsonData = json.loads(reqJson.text)
-		
-		for balance in jsonData['balance']:
-			if(balance['asset_hash'] == hashToFilter):
-				print(addressString + ' : ' + str(balance['amount']))
-				data.update({addressString : str(balance['amount'])})
+        if(pageNumber % 10 == 0):
+                print('Page number: ' + str(pageNumber))
+
+        for address in soup.find_all('a', attrs={'class':'large-blue-link col-4-width'}):
+                addressString = address.get_text()
+                reqJson = requests.get('https://api.neoscan.io/api/main_net/v1/get_balance/' + addressString, headers={'User-Agent': 'Mozilla/5.0'})
+                reqJson.encoding = 'UTF-8'
+                jsonData = json.loads(reqJson.text)
+
+                for balance in jsonData['balance']:
+                        if(balance['asset_hash'] == hashToFilter):
+                                if(float(balance['amount']) > 20000):
+                                        print(addressString + ' : ' + str(balance['amount']))
+                                        data.update({addressString : str(balance['amount'])})
 
 with open('balances.csv', 'w') as file:
-	w = csv.writer(file)
-	w.writerows(data.items())
-	
+        w = csv.writer(file)
+        w.writerows(data.items())
+
 sys.exit()
