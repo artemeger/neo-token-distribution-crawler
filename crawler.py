7a526273a47090deae32d23c30be24adaf12377a
Artem Eger <11612812+yuomii@users.noreply.github.com>
2019-01-06 11:12:58 +0100

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -9,26 +9,28 @@ pagesToScan = 121000
 hashToFilter = '45d493a6f73fa5f404244a5fb8472fc014ca5885'
 data = dict()
 
-for pageNumber in range (1, pagesToScan, 1):
+for pageNumber in range (pagesToScan):
 
         req = requests.get('https://neoscan.io/addresses/' + str(pageNumber), headers={'User-Agent': 'Mozilla/5.0'})
         soup = BeautifulSoup(req.text, 'html.parser')
 
-        if(pageNumber % 10 == 0):
+        if pageNumber % 10 == 0:
                 print('Page number: ' + str(pageNumber))
+                
         if soup is not None:
                 for address in soup.find_all('a', attrs={'class':'large-blue-link col-4-width'}):
                         addressString = address.get_text()
                         reqJson = requests.get('https://api.neoscan.io/api/main_net/v1/get_balance/' + addressString, headers={'User-Agent': 'Mozilla/5.0'})
-                        try:
-                                jsonData = reqJson.json()
-                                for balance in jsonData['balance']:
-                                        if(balance['asset_hash'] == hashToFilter):
-                                                if(float(balance['amount']) >= 20000):
-                                                        data.update({addressString : str(balance['amount'])})
-                                                        print(addressString + ' : ' + str(balance['amount']))
-                        except:
-                                print('Error processing Address: ' + addressString)
+                        if hashToFilter in reqJson.text:
+                                try:
+                                        jsonData = reqJson.json()
+                                        for balance in jsonData['balance']:
+                                                if balance['asset_hash'] == hashToFilter:
+                                                        if float(balance['amount']) >= 20000:
+                                                                data.update({addressString : str(balance['amount'])})
+                                                                print(addressString + ' : ' + str(balance['amount']))
+                                except:
+                                        print('Error processing Address: ' + addressString)
 
 with open('balances.csv', 'w') as file:
         w = csv.writer(file)
