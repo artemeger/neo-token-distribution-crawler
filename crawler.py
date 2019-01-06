f1cf37abdd5fc78a1f570e04c639ad2bf7dd3b0c
Artem Eger <11612812+yuomii@users.noreply.github.com>
2019-01-06 03:52:00 +0100

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -20,15 +20,15 @@ for pageNumber in range (1, pagesToScan, 1):
                 for address in soup.find_all('a', attrs={'class':'large-blue-link col-4-width'}):
                         addressString = address.get_text()
                         reqJson = requests.get('https://api.neoscan.io/api/main_net/v1/get_balance/' + addressString, headers={'User-Agent': 'Mozilla/5.0'})
-                        jsonData = reqJson.json()
-                        for balance in jsonData['balance']:
-                                if(balance['asset_hash'] == hashToFilter):
-                                        if(float(balance['amount']) >= 20000):
-                                                data.update({addressString : str(balance['amount'])})
-                                                if addressString not in data:
-                                                        with open("log.txt", "a") as myfile:
-                                                             myfile.write(addressString + ' : ' + str(balance['amount']))
-                                                             print(addressString + ' : ' + str(balance['amount']))
+                        try:
+                                jsonData = reqJson.json()
+                                for balance in jsonData['balance']:
+                                        if(balance['asset_hash'] == hashToFilter):
+                                                if(float(balance['amount']) >= 20000):
+                                                        data.update({addressString : str(balance['amount'])})
+                                                        print(addressString + ' : ' + str(balance['amount']))
+                        except:
+                                print('Error processing Address: ' + addressString)
 
 with open('balances.csv', 'w') as file:
         w = csv.writer(file)
